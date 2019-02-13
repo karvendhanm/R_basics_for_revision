7f5f0705167fce7fb2ab72839d51146718104dd8
Karvendhan <karvsmech@gmail.com>
2019-02-13 21:47:37 +0530

    Feature Addition: Installing and Running Swirl Course

---
+++ swrl_for_revision.R
@@ -0,0 +1,12 @@
+# installing and loading the library swirl
+install.packages('swirl')
+library('swirl')
+
+file_dir = 'C:/Users/John/Downloads/Data_Analysis/MITx/Data_Analysis_for_Social_Scientists'
+
+# installing a swirl course
+install_course_zip(paste(file_dir, "14_310x_Intro_to_R.zip", sep="/"), multi = FALSE)
+
+# starting swirl course
+swirl()
+
