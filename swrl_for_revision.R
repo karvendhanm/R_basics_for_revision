9375badfd7f9da3e26e3bb4b5fbec5d23d67652a
Karvendhan <karvsmech@gmail.com>
2019-02-15 07:52:40 +0530

    Feature Addition: Delete Directories

--- swrl_for_revision.R
+++ swrl_for_revision.R
@@ -1,5 +1,5 @@
 # installing and loading the library swirl
-install.packages('swirl')
+# install.packages('swirl')
 library('swirl')
 
 file_dir = 'C:/Users/John/Downloads/Data_Analysis/MITx/Data_Analysis_for_Social_Scientists'
@@ -10,3 +10,4 @@ install_course_zip(paste(file_dir, "14_310x_Intro_to_R.zip", sep="/"), multi = F
 # starting swirl course
 swirl()
 
+# unlink(recursive = TRUE) to delete directories
